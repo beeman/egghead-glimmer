bf3236d9daf01378101b0eb96fd321967cdfaddc
Bram Borggreve <borggreve@gmail.com>
2017-08-01 03:02:19 +0200

    template variables

--- src/ui/components/my-component/component.ts
+++ src/ui/components/my-component/component.ts
@@ -1,4 +1,14 @@
 import Component from '@glimmer/component';
 
 export default class MyComponent extends Component {
+
+  content = 'This is my Content!'
+
+  items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+  ]
+
 };
