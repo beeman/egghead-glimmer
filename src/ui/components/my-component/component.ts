9b7752650bf08a6320e47f2f277ac6ff992d9bd2
Bram Borggreve <borggreve@gmail.com>
2017-08-03 01:43:26 +0200

    dynamic-data

--- src/ui/components/my-component/component.ts
+++ src/ui/components/my-component/component.ts
@@ -1,14 +1,24 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class MyComponent extends Component {
 
-  content = 'This is my Content!'
+  @tracked content = 'This is my Content!'
 
-  items = [
+  @tracked items = [
     'Item 1',
     'Item 2',
     'Item 3',
     'Item 4',
   ]
+
+  updateContent() {
+    this.content = Date.now().toString()
+    this.items = [ ...this.items, this.content ]
+  }
+
+  constructor(options){
+    super(options)
+    setInterval(() => this.updateContent(), 1000)
+  }
 
 };
